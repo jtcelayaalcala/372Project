716be85f0091283973e95b1ff6eb1b91e56f8074
Jose Celaya-Alcala <jtcelayaalcala@email.arizona.edu>
2017-05-02 18:23:56 -0700

    Control Flow stuff

--- Person.jl
+++ Person.jl
@@ -27,10 +27,77 @@ nums = [1,2,3]
 
 println(length(randomStuff)) #find the length of the list
 
-push!("t",randomStuff) #append "t" to randomStuff
+push!(randomStuff, "t") #append "t" to randomStuff
 
 println(randomStuff) #print new list
 
 println(sum(nums)) #sum of all numbers in nums
 
 println(prod(nums)) #product of all numbers in nums
+
+i = 0
+
+while(i < 100)
+  i+=1
+end
+
+if i % 2 == 0
+  println(i)
+else
+  println(i+1)
+end
+
+
+for i = 1:20
+  println(i)
+end
+
+oddNums = [x for x in 1:50 if x % 2 == 1]
+
+squares = [x^2 for x in oddNums]
+
+
+i = 0
+doIf = true
+
+#while loop
+while i < 10
+  i+=1
+end
+
+#for loop
+for k = 1:5
+  println(k)
+end
+
+#if with an else
+if doIf
+  println("In the if!")
+else
+  println("Not in if!")
+end
+
+#if with an elseif
+if i == 10
+  println(i)
+elseif i < 10
+  print("i is bigger than 10!")
+end
+
+#short circuit evaluations
+if i == 10 || k < 100 #second part will not be evaluated since first is true
+  println("i < 10 or k < 100")
+end
+
+if i % 2 == 1 && k < 20 #second part not evaluated since first is false
+  println("i is odd or k < 20")
+end
+
+#ternary operator
+j = i < 10 ? 0 : 1 #if i < 10, j = 0, else j = 1
+
+z = begin
+    x = 1
+    y = 2
+    x + y
+    end
